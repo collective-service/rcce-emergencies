527d08511dcc60fb4ae2b0765d8a5d4c2359a71a
amadu <ndongamadou@gmail.com>
2022-06-09 13:32:49 +0100

    container et coloring
    container size, color from gs and legend

--- script.js
+++ script.js
@@ -2,20 +2,29 @@ const geodataUrl = 'world.json';
 const reliefWebApiURL = 'https://api.reliefweb.int/v1/disasters?appname=rwint-user-0&profile=list&preset=latest&fields[include][]=description';
 //https://api.reliefweb.int/v1/disasters?appname=rwint-user-0&profile=list&preset=latest&fields[include][]=description
 //https://api.reliefweb.int/v1/disasters?appname=rwint-user-0&profile=list&preset=latest&slim=1
+const colorsMappingURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTO4an4g3FxrhfbVptaxEB3GL7Gr3h3NMJOzZVyaJPslsFuXGJqYvQVkiawcXE6jXdi_uOB14cMQJxY/pub?gid=0&single=true&output=csv';
 
 let geomData,
-    emergenciesData;
-
+    emergenciesData,
+    colorsMappingArr;
+let legendEntries = [];
 $(document).ready(function() {
     function getData() {
         Promise.all([
             d3.json(geodataUrl),
             d3.json(reliefWebApiURL),
+            d3.csv(colorsMappingURL)
         ]).then(function(data) {
             geomData = topojson.feature(data[0], data[0].objects.geom);
 
             emergenciesData = api_cleanedOngoingDisasters(data[1].data);
 
+            colorsMappingArr = data[2];
+            colorsMappingArr.forEach(element => {
+                legendEntries.includes(element["Legend item"]) ? null : legendEntries.push(element["Legend item"]);
+            });
+            console.log(legendEntries);
+            console.log(emergenciesData);
             initiateMap();
             //remove loader and show vis
             $('.loader').hide();
@@ -30,12 +39,17 @@ function api_cleanedOngoingDisasters(apiData) {
     let dataArr = []; // id , countryname, iso3, name_emergencies, type=[]
     apiData.forEach(element => {
         const fields = element["fields"];
+        var types = [];
+        fields.type.forEach(t => {
+            types.push(t.name);
+        })
         dataArr.push({
             "id": element.id,
             "country": fields.country[0].name,
             "iso3": fields.glide.split('-')[3],
             "emergency": fields.name,
-            "types": [...fields.type],
+            "types": types,
+            "type_main": fields.type[0].name,
             "description": fields.description,
             "x": 0,
             "y": 0
@@ -67,15 +81,15 @@ let g, mapsvg, projection, width, height, zoom, path, maptip;
 let countriesISO3Arr = [];
 
 function initiateMap() {
-    width = viewportWidth;
+    width = document.getElementById("main").offsetWidth; //viewportWidth;
     height = (isMobile) ? 400 : 500;
-    var mapScale = (isMobile) ? width / 3.5 : width / 10.6;
+    var mapScale = (isMobile) ? width / 5.5 : width / 7.2;
     var mapCenter = (isMobile) ? [12, 12] : [25, 25];
 
     projection = d3.geoMercator()
         .center(mapCenter)
         .scale(mapScale)
-        .translate([width / 3.0, height / 1.9]);
+        .translate([width / 3.7, height / 1.9]);
 
     path = d3.geoPath().projection(projection);
     zoom = d3.zoom()
@@ -91,7 +105,7 @@ function initiateMap() {
         .on("dblclick.zoom", null);
 
     mapsvg.append("rect")
-        .attr("width", "100%")
+        .attr("width", width)
         .attr("height", "100%")
         // .attr("fill", "#d9d9d9");
         .attr("fill", "#1b365e"); //294780 //1b365e //cdd4d9
@@ -126,7 +140,7 @@ function initiateMap() {
         .attr('fill', function(d) {
             return countriesISO3Arr.includes(d.properties.ISO_A3) ? mapFillColor : mapInactive;
         })
-        .attr('stroke-width', 0)
+        .attr('stroke-width', 0.05)
         .attr('stroke', '#fff')
         .on("mousemove", function(d) {
             countriesISO3Arr.includes(d.properties.ISO_A3) ? mousemove(d) : null;
@@ -151,8 +165,9 @@ function initiateMap() {
         .append("g")
         .append("circle")
         .attr("class", "cercle")
-        .attr("r", 5)
+        .attr("r", 6)
         .attr("transform", function(d) { return "translate(" + [d.x, d.y] + ")"; })
+        .attr("fill", function(d) { return getColor(d.type_main); })
         .on("mousemove", function(d) {
             mousemove(d);
         })
@@ -175,15 +190,50 @@ function initiateMap() {
         zoom.scaleBy(mapsvg.transition().duration(500), 0.5);
     });
 
+    var legendSVG = d3.select('#legend').append("svg")
+        .attr("widht", "100%")
+        .attr("height", "100%");
+    const xcoord = 10;
+    legendSVG.append("g")
+        .selectAll("legend-item")
+        .data(legendEntries)
+        .enter()
+        .append("circle").attr("r", 6)
+        .attr("cx", xcoord)
+        .attr("cy", function(d, i) {
+            if (i == 0) {
+                return xcoord;
+            }
+            return xcoord + i * 25;
+        })
+        .attr("fill", function(legend) { return getColor(legend); });
+    legendSVG
+        .select("g")
+        .selectAll("text")
+        .data(legendEntries).enter()
+        .append("text")
+        .attr("x", xcoord * 2)
+        .attr("y", function(d, i) {
+            if (i == 0) {
+                return xcoord + 5;
+            }
+            return xcoord + 5 + i * 25;
+        })
+        .text(function(d) { return d; });
 } //initiateMap
 
 function mousemove(d) {
-    let countryName = (d.hasOwnProperty('properties')) ? d.properties.NAME : d.country;
-    const html = countryName +
-        '<br>' + "(click for more info)";
+    var html = "";
+
+    if (d.hasOwnProperty('properties')) {
+        const arr = emergenciesData.filter((e) => { return e.iso3 == d.properties.ISO_A3; });
+        html = arr[0].emergency;
+    } else html = d.emergency;
+
+    var mouse = d3.mouse(mapsvg.node()).map(function(d) { return parseInt(d); });
     maptip
         .classed('hidden', false)
-        .attr('style', 'left:' + (d3.event.pageX + 10) + 'px; top:' + (d3.event.pageY) + 'px')
+        .attr('style', 'left:' + (mouse[0] + 5) + 'px; top:' + (mouse[1] + 10) + 'px')
         .html(html);
 } //mousemove
 
@@ -205,4 +255,35 @@ function generateEmergencyInfo(filteredData) {
         filteredData.description;
 
     $("#emergency_desc").html(text);
+}
+
+function getColumnUniqueValues() {
+    var values = [];
+    for (let index = 0; index < arguments.length; index++) {
+        var arr = [];
+        values.push(arr);
+    }
+    emergenciesData.forEach(element => {
+        for (let index = 0; index < arguments.length; index++) {
+            var arr = element[arguments[index]].split(",");
+            var returnArr = values[index];
+            var trimedArr = arr.map(x => x.trim());
+            trimedArr.forEach(d => {
+                returnArr.includes(d.trim()) ? '' : returnArr.push(d.trim());
+            });
+            values[index] = returnArr;
+        }
+    });
+
+    return values;
+} //getColumnUniqueValues
+
+function getColor(type) {
+    var color = '#fff'
+    colorsMappingArr.forEach(element => {
+        if (element["RW type"].includes(type)) {
+            color = element.Color;
+        }
+    });
+    return color;
 }
